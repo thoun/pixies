dccae7c7d95f50bcc8c205e13985caaaeeeeb768
Guy <guy@boardgamearena.com>
2025-07-07 10:31:55 +0200

    migrate state declaration

--- states.inc.php
+++ states.inc.php
@@ -15,6 +15,7 @@
  */
 
 use Bga\GameFramework\GameStateBuilder;
+use Bga\GameFramework\StateType;
 
 /*
    Game state machine is a tool used to facilitate game developpement by doing common stuff that can be set up
@@ -33,7 +34,7 @@ use Bga\GameFramework\GameStateBuilder;
    Arguments of game states:
    _ name: the name of the GameState, in order you can recognize it on your own code.
    _ description: the description of the current game state is always displayed in the action status bar on
-                  the top of the game. Most of the time this is useless for game state with "game" type.
+                  the top of the game. Most of the time this is useless for game state with StateType::GAME type.
    _ descriptionmyturn: the description of the current game state when it's your turn.
    _ type: defines the type of game states (activeplayer / multipleactiveplayer / game / manager)
    _ action: name of the method to call when this game state become the current game state. Usually, the
@@ -59,141 +60,141 @@ $basicGameStates = [
 
 $playerActionsGameStates = [
 
-    ST_PLAYER_CHOOSE_CARD => [
-        "name" => "chooseCard",
-        "description" => clienttranslate('${actplayer} must choose a card to play'),
-        "descriptionmyturn" => clienttranslate('${you} must choose a card to play'),
-        "type" => "activeplayer",
-        "args" => "argChooseCard",  
-        "possibleactions" => [ 
+    ST_PLAYER_CHOOSE_CARD => GameStateBuilder::create()
+        ->name("chooseCard")
+        ->description(clienttranslate('${actplayer} must choose a card to play'))
+        ->descriptionmyturn(clienttranslate('${you} must choose a card to play'))
+        ->type(StateType::ACTIVE_PLAYER)
+        ->args("argChooseCard")  
+        ->possibleactions([ 
             'actChooseCard',
-        ],
-        "transitions" => [
+        ])
+        ->transitions([
             "playCard" => ST_PLAYER_PLAY_CARD,
             "keepCard" => ST_PLAYER_KEEP_CARD,
             "zombiePass" => ST_NEXT_PLAYER,
-        ]
-    ],
+        ])
+        ->build(),
 
-    ST_PLAYER_PLAY_CARD => [
-        "name" => "playCard",
-        "description" => clienttranslate('${actplayer} must place the card'),
-        "descriptionmyturn" => clienttranslate('${you} must place the card'),
-        "type" => "activeplayer",    
-        "args" => "argPlayCard",    
-        "possibleactions" => [ 
+    ST_PLAYER_PLAY_CARD => GameStateBuilder::create()
+        ->name("playCard")
+        ->description(clienttranslate('${actplayer} must place the card'))
+        ->descriptionmyturn(clienttranslate('${you} must place the card'))
+        ->type(StateType::ACTIVE_PLAYER)    
+        ->args("argPlayCard")    
+        ->possibleactions([ 
             'actChooseCard',
             'actPlayCard',
             'actCancel',
-        ],
-        "transitions" => [
+        ])
+        ->transitions([
             "next" => ST_NEXT_PLAYER,
             "cancel" => ST_PLAYER_CHOOSE_CARD,
             "zombiePass" => ST_NEXT_PLAYER,
-        ]
-    ],
+        ])
+        ->build(),
 
-    ST_PLAYER_KEEP_CARD => [
-        "name" => "keepCard",
-        "description" => clienttranslate('${actplayer} must choose a card to keep'),
-        "descriptionmyturn" => clienttranslate('${you} must choose a card to keep'),
-        "type" => "activeplayer", 
-        "args" => "argKeepCard",    
-        "possibleactions" => [ 
+    ST_PLAYER_KEEP_CARD => GameStateBuilder::create()
+        ->name("keepCard")
+        ->description(clienttranslate('${actplayer} must choose a card to keep'))
+        ->descriptionmyturn(clienttranslate('${you} must choose a card to keep'))
+        ->type(StateType::ACTIVE_PLAYER) 
+        ->args("argKeepCard")    
+        ->possibleactions([ 
             'actChooseCard',
             'actKeepCard',
             'actCancel',
-        ],
-        "transitions" => [
+        ])
+        ->transitions([
             "next" => ST_NEXT_PLAYER,
             "cancel" => ST_PLAYER_CHOOSE_CARD,
             "zombiePass" => ST_NEXT_PLAYER,
-        ]
-    ],
+        ])
+        ->build(),
     
-    ST_MULTIPLAYER_BEFORE_END_ROUND => [
-        "name" => "beforeEndRound",
-        "description" => clienttranslate('Some players are seeing end round result'),
-        "descriptionmyturn" => clienttranslate('End round result'),
-        "type" => "multipleactiveplayer",
-        "action" => "stBeforeEndRound",
-        "possibleactions" => [ 
+    ST_MULTIPLAYER_BEFORE_END_ROUND => GameStateBuilder::create()
+        ->name("beforeEndRound")
+        ->description(clienttranslate('Some players are seeing end round result'))
+        ->descriptionmyturn(clienttranslate('End round result'))
+        ->type(StateType::MULTIPLE_ACTIVE_PLAYER)
+        ->action("stBeforeEndRound")
+        ->possibleactions([ 
             'actSeen',
-        ],
-        "transitions" => [
+        ])
+        ->transitions([
             "next" => ST_END_ROUND, // for zombie
             "endRound" => ST_END_ROUND,
             "endScore" => ST_END_SCORE,
-        ],
-    ],
+        ])
+        ->build(),
 ];
 
 $gameGameStates = [
 
-    ST_NEW_ROUND => [
-        "name" => "newRound",
-        "description" => "",
-        "type" => "game",
-        "action" => "stNewRound",
-        "updateGameProgression" => true,
-        "transitions" => [
+    ST_NEW_ROUND => GameStateBuilder::create()
+        ->name("newRound")
+        ->description("")
+        ->type(StateType::GAME)
+        ->action("stNewRound")
+        ->updateGameProgression(true)
+        ->transitions([
             "start" => ST_NEW_TURN,
-        ],
-    ],
+        ])
+        ->build(),
 
-    ST_NEW_TURN => [
-        "name" => "newTurn",
-        "description" => "",
-        "type" => "game",
-        "action" => "stNewTurn",
-        "updateGameProgression" => true,
-        "transitions" => [
+    ST_NEW_TURN => GameStateBuilder::create()
+        ->name("newTurn")
+        ->description("")
+        ->type(StateType::GAME)
+        ->action("stNewTurn")
+        ->updateGameProgression(true)
+        ->transitions([
             "start" => ST_PLAYER_CHOOSE_CARD,
-        ],
-    ],
+        ])
+        ->build(),
 
-    ST_NEXT_PLAYER => [
-        "name" => "nextPlayer",
-        "description" => "",
-        "type" => "game",
-        "action" => "stNextPlayer",
-        "transitions" => [
+    ST_NEXT_PLAYER => GameStateBuilder::create()
+        ->name("nextPlayer")
+        ->description("")
+        ->type(StateType::GAME)
+        ->action("stNextPlayer")
+        ->transitions([
             "next" => ST_PLAYER_CHOOSE_CARD, 
             "endTurn" => ST_END_TURN,
-        ],
-    ],
+        ])
+        ->build(),
 
-    ST_END_TURN => [
-        "name" => "endTurn",
-        "description" => "",
-        "type" => "game",
-        "action" => "stEndTurn",
-        "transitions" => [
+    ST_END_TURN => GameStateBuilder::create()
+        ->name("endTurn")
+        ->description("")
+        ->type(StateType::GAME)
+        ->action("stEndTurn")
+        ->transitions([
             "newTurn" => ST_NEW_TURN,
             "endRound" => ST_MULTIPLAYER_BEFORE_END_ROUND,
-        ],
-    ],
+        ])
+        ->build(),
 
-    ST_END_ROUND => [
-        "name" => "endRound",
-        "description" => "",
-        "type" => "game",
-        "action" => "stEndRound",
-        "transitions" => [
+    ST_END_ROUND => GameStateBuilder::create()
+        ->name("endRound")
+        ->description("")
+        ->type(StateType::GAME)
+        ->action("stEndRound")
+        ->transitions([
             "newRound" => ST_NEW_ROUND,
             "endScore" => ST_END_SCORE,
-        ],
-    ],
+        ])
+        ->build(),
 
-    ST_END_SCORE => [
-        "name" => "endScore",
-        "description" => "",
-        "type" => "game",
-        "action" => "stEndScore",
-        "transitions" => [
+    ST_END_SCORE => GameStateBuilder::create()
+        ->name("endScore")
+        ->description("")
+        ->type(StateType::GAME)
+        ->action("stEndScore")
+        ->transitions([
             "endGame" => ST_END_GAME,
-        ],
-    ],
+        ])
+        ->build(),
 ];
  
 $machinestates = $basicGameStates + $playerActionsGameStates + $gameGameStates;
